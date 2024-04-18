566fabae316c2594230063528e3dd1802cf9a1c2
unknown <leeduchht@gmail.com>
2024-04-18 17:26:01 +0700

    Create Project

---
+++ src/Component/header/header.component.tsx
@@ -0,0 +1,14 @@
+'use client'
+import Container from '@mui/material/Container';
+export default function AppHeader() {
+
+    return (
+        <Container>
+            <div>
+                header layout
+
+            </div>
+        </Container>
+    )
+
+}
